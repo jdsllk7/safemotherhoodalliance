52fabcda4682d70c82436c93caf05a059ff7fbae
KOSAMUJ <kosamuk@zra.org.zm>
2021-01-12 10:58:02 +0200

    qwerty

--- index.php
+++ index.php
@@ -524,12 +524,12 @@
 			<div class="row mb-5">
 				<div class="col-sm-12 col-md">
 					<div class="ftco-footer-widget mb-4">
-						<h2 class="ftco-heading-2 logo"><a href="#">Counselor</a></h2>
-						<p>Far far away, behind the word mountains, far from the countries.</p>
+						<h2 class="ftco-heading-2 logo"><a href="index.php">Safe Motherhood Alliance</a></h2>
+						<p>Improving maternal and child health in Zambia</p>
 						<ul class="ftco-footer-social list-unstyled mt-2">
-							<li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
-							<li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
-							<li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
+							<li class="ftco-animate"><a target="_blank" title="Our Twitter Page" href="https://twitter.com/alliance_safe"><span class="fa fa-twitter"></span></a></li>
+							<li class="ftco-animate"><a target="_blank" title="Our Facebook Page" href="https://www.facebook.com/safemotherhoodkits/"><span class="fa fa-facebook"></span></a></li>
+							<li class="ftco-animate"><a target="_blank" title="Our LinkedIn Page" href="https://www.linkedin.com/company/safe-motherhood-alliance/"><span class="fa fa-linkedin"></span></a></li>
 						</ul>
 					</div>
 				</div>
@@ -537,43 +537,32 @@
 					<div class="ftco-footer-widget mb-4 ml-md-4">
 						<h2 class="ftco-heading-2">Explore</h2>
 						<ul class="list-unstyled">
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>About</a></li>
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Contact</a></li>
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>What We Do</a></li>
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Plans &amp; Pricing</a></li>
+							<li><a href="about.php"><span class="fa fa-chevron-right mr-2"></span>About Us</a></li>
+							<li><a href="contact.php"><span class="fa fa-chevron-right mr-2"></span>Contact Us</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Our Blog</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Subscribe</a></li>
 						</ul>
 					</div>
 				</div>
 				<div class="col-sm-12 col-md">
 					<div class="ftco-footer-widget mb-4 ml-md-4">
-						<h2 class="ftco-heading-2">Legal</h2>
+						<h2 class="ftco-heading-2">Services</h2>
 						<ul class="list-unstyled">
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Join us</a></li>
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Blog</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Delivery Kits</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Other</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Other</a></li>
 							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Privacy &amp; Policy</a></li>
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Term &amp; Conditions</a></li>
 						</ul>
 					</div>
 				</div>
 				<div class="col-sm-12 col-md">
 					<div class="ftco-footer-widget mb-4">
-						<h2 class="ftco-heading-2">Company</h2>
-						<ul class="list-unstyled">
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>About Us</a></li>
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Blog</a></li>
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Contact</a></li>
-							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Careers</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-sm-12 col-md">
-					<div class="ftco-footer-widget mb-4">
-						<h2 class="ftco-heading-2">Have a Questions?</h2>
+						<h2 class="ftco-heading-2">Reach us</h2>
 						<div class="block-23 mb-3">
 							<ul>
-								<li><span class="icon fa fa-map marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-								<li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-								<li><a href="#"><span class="icon fa fa-paper-plane pr-4"></span><span class="text">info@yourdomain.com</span></a></li>
+								<li><a target="_blank" href="http://maps.google.com/?q=Plot 18 Mundulundulu Village, Siavonga, Lusaka, Zambia"><span class="icon fa fa-map marker"></span><span class="text">Plot 18 Mundulundulu Village, Siavonga. Lusaka, Zambia.</span></a></li>
+								<li><a href="tel:+260977748759" title="Call us"><span class="icon fa fa-phone"></span><span class="text">+26 0977 748759</span></a></li>
+								<li><a target="_blank" title="Email us" href="mailto:info@safemotherhoodalliance.org?Subject=Hello"><span class="icon fa fa-paper-plane pr-4"></span><span class="text">info@safemotherhoodalliance.org</span></a></li>
 							</ul>
 						</div>
 					</div>
@@ -584,12 +573,14 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
-
 						<p class="mb-0" style="color: rgba(255,255,255,.5);">
 							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 							Copyright &copy;<script>
 								document.write(new Date().getFullYear());
-							</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+							</script> All rights reserved | Powered by
+							<a href="https://joshuakosamu.netlify.app/" target="_blank">
+								<img src="https://jdslk.netlify.com/other/images/jdslk_logo.png" height="40px" width="40px" alt="joshuakosamu.netlify.app">
+							</a>
 							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 						</p>
 					</div>
