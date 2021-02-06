6ca8569ac158602966768c126d2d5510cf7afdd4
KOSAMUJ <kosamuk@zra.org.zm>
2021-02-06 18:33:40 +0200

    sean

--- includes/class-autoLoader.inc.php
+++ includes/class-autoLoader.inc.php
@@ -6,9 +6,9 @@ function myAutoLoader($className)
     $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
     if (strpos($url, 'includes') !== false) {
-        $path = "..\classes\\";
+        $path = "../classes/";
     } else {
-        $path = "classes\\";
+        $path = "classes/";
     }
     $extension = ".class.php";
     $fullPath = $path . $className . $extension;
