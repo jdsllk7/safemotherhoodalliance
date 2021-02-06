afe96d6e8857877406099e8bb1c4a71fd3a76ee2
KOSAMUJ <kosamuk@zra.org.zm>
2021-02-06 21:27:24 +0200

    qwerty

--- includes/class-autoLoader.inc.php
+++ includes/class-autoLoader.inc.php
@@ -5,18 +5,23 @@ function myAutoLoader($className)
 {
     $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
-    if (strpos($url, 'includes') !== false) {
-        $path = "../classes/";
-    } else {
-        $path = "classes/";
+    if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) {
+        if (strpos($url, 'includes') !== false) {
+            $path = "../classes/";
+        } else {
+            $path = "classes/";
+        }
+    } elseif (strpos($_SERVER['HTTP_HOST'], 'safemotherhoodalliance.org') !== false) {
+        $path = "/home/dembebxq/public_html/classes/";
     }
+
     $extension = ".class.php";
     $fullPath = $path . $className . $extension;
 
-    var_dump(file_exists($fullPath));
+    var_dump(file_exists(strtolower($fullPath)));
     var_dump($fullPath);
-    
-    if (!file_exists($fullPath)) {
+
+    if (!file_exists(strtolower($fullPath))) {
         return false;
     } else {
         // var_dump($fullPath);
